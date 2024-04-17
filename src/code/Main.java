11286da4c883b4b9ed345db45cfa908d12bca734
MarcusBigTreeBig <the.marcus.herbert@gmail.com>
2024-04-16 23:40:01 -0700

    added sample inputs

--- src/code/Main.java
+++ src/code/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner input = new Scanner(new File("src/input_files/input.txt"));
+        Scanner input = new Scanner(new File("src/input_files/demo.txt"));
         Particle.setUsesGUI(input.nextInt() == 1);
         Particle.setRunTime(input.nextInt());
         Particle.setTickSpeed(input.nextLong());
